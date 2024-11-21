6e22ed798a984da2e159fecbe7bc03c8f77c5774
LeHien6601 <lehien6601@gmail.com>
2024-11-21 20:07:51 +0700

    Update green background

---
+++ Assets/Scripts/BackgroundFollowCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFollowCamera : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    private float initialCamXPos;
+    private float initialXPos;
+    [SerializeField] float speed;
+    private void Start()
+    {
+        initialCamXPos = cam.transform.position.x;
+        initialXPos = transform.position.x;
+    }
+    private void Update()
+    {
+        transform.position = new Vector2(initialXPos + speed * (cam.transform.position.x - initialCamXPos), transform.position.y);
+    }
+}
