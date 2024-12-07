202ebcd8d7a1e034b838d4b78116a58d4dc9d9cd
trannhatdang <77575859+trannhatdang@users.noreply.github.com>
2024-12-07 10:21:17 +0700

    revert finish gatye

--- Assets/Scripts/FinishGate.cs
+++ Assets/Scripts/FinishGate.cs
@@ -37,49 +37,49 @@ public class FinishGate : MonoBehaviour
         }
         transform.localScale = Vector3.one + Vector3.one * (timer + 1f) * 0.5f;
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision == null) return;
-        if(collision.tag != "Player") return;
-
-        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
-        rb.bodyType = RigidbodyType2D.Kinematic;
-        rb.velocity = Vector3.zero;
-        player = collision.gameObject.GetComponent<Player>();
-        player.GetAnimator().SetTrigger("Win");
-        StartCoroutine(finish());
-    }
-    IEnumerator finish()
-    {
-        float speed = 15f;
-        Vector2 direction = transform.position - player.transform.position;
-        for(int i = 0; i < 15; ++i)
-        {
-            player.gameObject.GetComponent<Rigidbody>().position += Vector3.ClampMagnitude(direction, speed);
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
-    // private void OnTriggerStay2D(Collider2D collision)
+    // private void OnTriggerEnter2D(Collider2D collision)
     // {
-    //     if (collision == null) return;
-    //     if (collision.tag != "Player") return;
+    //     if(collision == null) return;
+    //     if(collision.tag != "Player") return;
+
     //     Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
     //     rb.bodyType = RigidbodyType2D.Kinematic;
     //     rb.velocity = Vector3.zero;
-    //     Vector3 direction = transform.position - collision.transform.position;
-    //     if (direction.magnitude < circleCollider.radius / 2)
+    //     player = collision.gameObject.GetComponent<Player>();
+    //     player.GetAnimator().SetTrigger("Win");
+    //     StartCoroutine(finish());
+    // }
+    // IEnumerator finish()
+    // {
+    //     float speed = 15f;
+    //     Vector2 direction = transform.position - player.transform.position;
+    //     for(int i = 0; i < 15; ++i)
     //     {
-    //         rb.MovePosition(transform.position);
-    //         circleCollider.isTrigger = false;
-    //         Time.timeScale = 1f;
-    //         //Finish game!!!!!!
-    //         GameController.instance.LoadNextLevel();
-    //     }
-    //     else
-    //     {
-    //         rb.velocity = Vector2.zero;
-    //         rb.position += Vector2.ClampMagnitude(direction, attraction);
-    //         Time.timeScale = 1f;
+    //         player.gameObject.GetComponent<Rigidbody>().position += Vector3.ClampMagnitude(direction, speed);
+    //         yield return new WaitForSeconds(0.1f);
     //     }
     // }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision == null) return;
+        if (collision.tag != "Player") return;
+        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.velocity = Vector3.zero;
+        Vector3 direction = transform.position - collision.transform.position;
+        if (direction.magnitude < circleCollider.radius / 2)
+        {
+            rb.MovePosition(transform.position);
+            circleCollider.isTrigger = false;
+            Time.timeScale = 1f;
+            //Finish game!!!!!!
+            GameController.instance.LoadNextLevel();
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+            rb.position += Vector2.ClampMagnitude(direction, attraction);
+            Time.timeScale = 1f;
+        }
+    }
 }
